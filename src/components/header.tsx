af14c2f4d28d31c2421c612763b62af23b363f06
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-12 20:08:05 +0500

    blogging

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+
+
+const Header = () =>{
+    return <header className="bg-blue-500 p-4">
+        <nav className='flex justify-between items-center max-w-4xl mx-auto'>
+            <Link href='/' className='text-white text-2xl font-bold'>
+            My Blogs</Link>
+
+        </nav>
+        </header>;
+
+};
+
+export default Header;
