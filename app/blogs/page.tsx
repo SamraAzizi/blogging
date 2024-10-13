b0e58c9b86ccd9ad29b6f096bfc51e590e6753ba
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-13 21:14:07 +0500

    blogging

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -12,9 +12,7 @@ const BlogsPage = () =>{
         </div>
 
         {posts.map((post) =>(
-            <a key ={post.id} href={`/blogs/${post.id}`} className='bg-white p-4 rounded-md shadow'>
-                <h2 className='text-xl font-bold'>{post.title}</h2>
-                <p>Written by: {post.username}</p>
+            <a key ={post.id} hre
             </a>
         ))}
         
