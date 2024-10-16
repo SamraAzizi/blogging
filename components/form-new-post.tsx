7637d3f9a7e480fee9764b75b64a10f88cfab99c
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-16 10:24:39 +0500

    blogging

--- components/form-new-post.tsx
+++ components/form-new-post.tsx
@@ -1,78 +1,79 @@
 'use client';
-import React, { ChangeEvent, FormEvent, useState } from "react"; // Import both ChangeEvent and useState
-import TextareaAutosize from "react-textarea-autosize"; // Import here
 
-// Define the FormData interface
-interface FormData {
-  title: string;
-  content: string;
-}
+import { FormData } from '@/types/blog';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import ReactTextareaAutosize from 'react-textarea-autosize';
 
-const inputClass = 
+const inputClass =
   'w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300';
 
 const FormNewPost = () => {
-    const [formData, setFormData] = useState<FormData>({
-        title: '',
-        content: ''
+  const [formData, setFormData] = useState<FormData>({
+    title: '',
+    content: '',
+  });
+  const { data } = useSession();
+  const router = useRouter();
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
-    const { data } useSession();
-    const router  = useRouter();
+  };
 
-    // Handle input changes for both text input and textarea
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target; // Destructure name and value from the target
-        setFormData(prev => ({ ...prev, [name]: value })); // Update state
-    };
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    // Single handleSubmit function
-    const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Prevent default form submission
-        // Handle form submission logic here, e.g., sending data to a server
-        try{
-            const response = await axios.post('api/posts', formData);
-            if(response.status === 200)
-                router.push(`/blogs/${response.data.newPost.id}`)
+    try {
+      const response = await axios.post('api/posts', formData);
 
-        } catch(error){
-            console.error(error);
+      if (response.status === 200) {
+        router.push(`/blogs/${response.data.newPost.id}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        }
-    };
-
-    return (
-        <form className="max-w-4xl mx-auto p-4" 
-        onSubmit={handleSubmit}>
-            <div className="mb-4">
-                <input 
-                    type="text" 
-                    className={inputClass}
-                    placeholder='Enter the Title'
-                    name='title'
-                    value={formData.title}
-                    onChange={handleChange}
-                />
-            </div>
-
-            <div className="mb-4">
-                <TextareaAutosize 
-                    minRows={5} 
-                    name='content'
-                    className={inputClass}
-                    placeholder="Enter the content"
-                    value={formData.content}
-                    onChange={handleChange}
-                />
-            </div>
-
-            <button 
-                type="submit" 
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full disabled:bg-gray-400"
-            >
-                Submit
-            </button>
-        </form>
-    );
+  return (
+    <form className='max-w-md mx-auto p-4' onSubmit={handleSubmit}>
+      <div className='mb-4'>
+        <input
+          type='text'
+          className={inputClass}
+          placeholder='Enter the title'
+          name='title'
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='mb-4'>
+        <ReactTextareaAutosize
+          minRows={5}
+          name='content'
+          className={inputClass}
+          placeholder='Enter the content'
+          value={formData.content}
+          onChange={handleChange}
+        />
+      </div>
+      <button
+        disabled={!data?.user?.email}
+        type='submit'
+        className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full disabled:bg-gray-400'
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default FormNewPost;
