607071ae241a0f64a33ed5a6a315de95e0216474
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-17 22:08:32 +0500

    blogging

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -10,16 +10,7 @@ interface BlogDetailPageProps {
 }
 
 
-  return (
-    <div className='max-w-4xl mx-auto py-8'>
-      <h1 className='text-3xl font-bold'>{post?.title}</h1>
-      <p>Written by: {post?.author?.name}</p>
-      <div className='mt-4'>{post?.content}</div>
-
-      <Comments postId={params.id} />
-      <FormComment postId={params.id} />
-    </div>
-  );
+  
 };
 
 export default BlogDetailPage;
